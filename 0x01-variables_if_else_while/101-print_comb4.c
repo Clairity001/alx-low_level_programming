cfc775f45cabc448d54399a679b8294c1ac1706c
Clairity <Ojingwaanayo@gmail.com>
2022-06-04 13:46:39 -0700

    Task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,10 +14,10 @@ int main(void)
 	while (e < 58)
 	{
 		i = 48;
-		while (i < 58);
+		while (i < 58)
 		{
 			g = 48;
-			while (g < 58);
+			while (g < 58)
 			{
 				if (e != i && e != g && i != g && e < i && i < g)
 				{
