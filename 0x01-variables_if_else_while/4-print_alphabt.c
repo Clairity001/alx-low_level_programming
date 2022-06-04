c390a96d54e3059419473ce73c23059532a08f8b
Clairity <Ojingwaanayo@gmail.com>
2022-06-03 19:55:54 -0700

    Tas

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,6 +12,8 @@ int main(void)
 
 {
 	int i;
+
+
 	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
@@ -19,6 +21,7 @@ int main(void)
 			putchar(i);
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
