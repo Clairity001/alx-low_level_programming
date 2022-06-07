aad04aad688058d8e9d7fa500d76dcebbeeb3e31
Clairity <Ojingwaanayo@gmail.com>
2022-06-07 15:20:23 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Function that prints alphabets
+ * Descripion: to print alphabets
+ * Descripion: to print alphabets
+ */
+void print_alphabet(void)
+{
+	int count;
 
-#include "main.h"#include <s
+	for (count = 'a'; count <= 'z'; count++)
+	{
+		_putchar(count);
+	}
+	_putchar('\n');
+}
