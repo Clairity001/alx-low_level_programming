8dc42e880cc6429f6d599d045c8e605d1d2ca42b
Clairity <Ojingwaanayo@gmail.com>
2022-06-07 16:02:22 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _isalpha - This program checks for alphabetic character 
+ *_isalpha - This program checks for alphabetic character 
  * @c: input character
  * Return: return 1 if c is lower and 0 if otherwise
  */
