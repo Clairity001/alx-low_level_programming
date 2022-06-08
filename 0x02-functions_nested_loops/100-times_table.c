173e98c030c0775a1089440bf984536244da48ff
Clairity <Ojingwaanayo@gmail.com>
2022-06-08 02:08:37 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,7 @@ void print_times_table(int n)
 					}
 					else if (op <= 99)
 					{
-						_putchar(32)
+						_putchar(32);
 						_putchar((op / 10) + 48);
 						_putchar((op / 10) + 48);
 					}
