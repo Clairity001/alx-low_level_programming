b85de3ff80b5a4b491663ed3e0646c25ad4bc0d2
Clairity <Ojingwaanayo@gmail.com>
2022-06-09 04:30:43 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is negative or positive
+ * Return: )
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative (i);
+
+	return (0);
+}
