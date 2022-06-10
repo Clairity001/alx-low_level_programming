0dfa800c6807fdff2b4e89550893f762d97295c8
Clairity <Ojingwaanayo@gmail.com>
2022-06-10 08:00:15 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,21 +4,14 @@
  * Description: prints the number excluding 2 and 4
  * Return: The numbers since 0 up to 9
  */
-
 void print_most_numbers(void)
 {
 	int x = 0;
 
-	for (; x <= 9; x++)
+	for (x = 0; x < 10; x++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
+		if (x != 2 &&  x != 4)
 			_putchar(x + '0');
-		}
 	}
 	_putchar('\n')
 }
