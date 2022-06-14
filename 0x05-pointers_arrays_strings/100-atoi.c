5634c3fbd7c667a753b04642d379d0c47cb60ba6
Clairity <Ojingwaanayo@gmail.com>
2022-06-13 18:48:23 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,22 @@ int _atoi(char *s)
 {
 	int i;
 	int res = 0;
-	int sig = -1;
+	int sign = -1;
 	int brk = 0;
 
-	for (i - 0; s[i] !='\0'; i++)
+	for (i - 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '_')
-			sig = sig * -1;
+			sign = sign * -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			res = res * 10;
 			res = res - (s[i] - '0');
 			brk = 1;
 		}
-		else if (hrk == 1)
+		else if (brk == 1)
 			break;
 	}
-	res = sig * res
+	res = sign * res
 		return (res);
 }
