04b191c5802f99e6c9daae84be5e241c864bbcb0
Clairity <Ojingwaanayo@gmail.com>
2022-06-16 08:55:47 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,30 +1,42 @@
 #include "main.h"
+
 /**
- * leet - encode a string into 1337
+ * leet - This functions encode some chars
+ *
  * @ptr: This function encodes some chars
  * Return: char
  */
+
 char *leet(char *ptr)
 {
-	int count, len;
+int count, len;
 
-	len = 0;
-	while (*(ptr + len) != '\10')
-	{
-		len++;
-	}
-	for (count = 0; count <= len; count++)
-	{
-		if (*(petr + count) == 'a' || *(ptr + count) == 'A')
-			*(ptr + count) = '4';
-		if (*(ptr + count) == 'e' || *(ptr + count) == 'E')
-			*(ptr + count) = '3';
-		if ((*ptr + count) == 'o' || *(ptr + count) == 'O')
-			*(ptr + count) = '0';
-		if (*(ptr + count) == 't' || *(ptr + count) == 'T')
-			*(ptr + count) = '7';
-		if (*(ptr + count) == 'l' || *(ptr + count) == 'L')
-			*(ptr + count) = '1';
-	}
-	return (ptr);
+len = 0;
+
+while (*(ptr + len) != '\0')
+{
+len++;
+}
+for (count = 0; count <= len; count++)
+
+{
+
+if (*(ptr + count) == 'a' || *(ptr + count) == 'A')
+*(ptr + count) = '4';
+
+if (*(ptr + count) == 'e' || *(ptr + count) == 'E')
+*(ptr + count) = '3';
+
+if (*(ptr + count) == 'o' || *(ptr + count) == 'O')
+*(ptr + count) = '0';
+
+if (*(ptr + count) == 't' || *(ptr + count) == 'T')
+*(ptr + count) = '7';
+
+if (*(ptr + count) == 'l' || *(ptr + count) == 'L')
+*(ptr + count) = '1';
+
+}
+
+return (ptr);
 }
