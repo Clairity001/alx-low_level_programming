be77879d6bb06d8fc4e241ab5f6b066bacbc3650
Clairity <Ojingwaanayo@gmail.com>
2022-06-16 09:01:26 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,34 +9,26 @@
 
 char *leet(char *ptr)
 {
-int count, len;
+	int count, len;
 
-len = 0;
+	len = 0;
 
-while (*(ptr + len) != '\0')
-{
-len++;
-}
-for (count = 0; count <= len; count++)
-
-{
-
-if (*(ptr + count) == 'a' || *(ptr + count) == 'A')
-*(ptr + count) = '4';
-
-if (*(ptr + count) == 'e' || *(ptr + count) == 'E')
-*(ptr + count) = '3';
-
-if (*(ptr + count) == 'o' || *(ptr + count) == 'O')
-*(ptr + count) = '0';
-
-if (*(ptr + count) == 't' || *(ptr + count) == 'T')
-*(ptr + count) = '7';
-
-if (*(ptr + count) == 'l' || *(ptr + count) == 'L')
-*(ptr + count) = '1';
-
-}
-
-return (ptr);
+	while (*(ptr + len) != '\0')
+	{
+		len++;
+	}
+	for (count = 0; count <= len; count++)
+	{
+		if (*(ptr + count) == 'a' || *(ptr + count) == 'A')
+			*(ptr + count) = '4';
+		if (*(ptr + count) == 'e' || *(ptr + count) == 'E')
+			*(ptr + count) = '3';
+		if (*(ptr + count) == 'o' || *(ptr + count) == 'O')
+			*(ptr + count) = '0';
+		if (*(ptr + count) == 't' || *(ptr + count) == 'T')
+			*(ptr + count) = '7';
+		if (*(ptr + count) == 'l' || *(ptr + count) == 'L')
+			*(ptr + count) = '1';
+	}
+	return (ptr);
 }
